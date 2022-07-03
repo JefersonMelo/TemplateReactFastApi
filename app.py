b78bf15e4d624286fc854c4e58f0f8d93b928c53
Jeferson Melo <jefsdemelo@gmail.com>
2022-07-03 17:16:36 -0300

    feat(route user): get user by user id

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from api.api import api
+from fastapi import FastAPI, Request
+
+# App Instance
+app = FastAPI()
+
+# Register Api Routers
+app.include_router(api)
+
+# Cors support - Api open to public
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+# Gzip compression support
+app.add_middleware(GZipMiddleware)
+
+# Show React build files
+app.mount('/static', StaticFiles(directory='app/build/static'), name='static')
+templates = Jinja2Templates(directory='app/build')
+
+
+# Serves Single Page Application files (eg: React)
+@app.get('/{full_path:path}')
+async def serve_spa(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
