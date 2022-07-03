80daff28847dd235c04dedb117093c6b22aed518
Jeferson Melo <jefsdemelo@gmail.com>
2022-07-03 17:31:25 -0300

    feat(date formate): date utc-3

--- api/helpers/helpers.py
+++ api/helpers/helpers.py
@@ -18,3 +18,20 @@ class FormatHelper:
         except Exception as e:
             return None, str(e)
 
+    @classmethod
+    def date_utc_3(cls, date: datetime) -> Tuple[Optional[datetime], str]:
+        try:
+
+            t_delta = timedelta(hours=-3)
+            utc_3 = timezone(t_delta)
+            date.astimezone(utc_3)
+
+            if not date:
+                return None, 'Erro ao Ajustar data'
+
+            return date, 'Date Belezinha'
+
+        except Exception as e:
+            return None, str(e)
+
+
