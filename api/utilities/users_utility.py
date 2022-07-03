2683087ae455498013d87c906ef59cb752b35cc0
Jeferson Melo <jefsdemelo@gmail.com>
2022-07-03 17:45:57 -0300

    feat(User Utility): Get user from user id

--- api/utilities/users_utility.py
+++ api/utilities/users_utility.py
@@ -36,4 +36,26 @@ class UsersUtility:
         except Exception as e:
             return None, str(e)
 
+    def get_user(
+            self,
+            user_id: int
+    ) -> Tuple[Optional[User], str]:
 
+        try:
+            with self.session_maker() as session:
+
+                results, msg = self.users.get_user_id(
+                    user_id=user_id,
+                    db=session
+                )
+
+                if not results:
+                    session.rollback()
+                    return None, msg
+
+                session.expunge_all()
+
+                return results, msg
+
+        except Exception as e:
+            return None, str(e)
